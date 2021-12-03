3766cf2f166cf827a35ed8f5f5b0518cdc764112
Manuel Verweyen <manuel.verweyen@gmail.com>
2021-12-03 14:13:50 +0100

    team seite api done

--- src/teams.js
+++ src/teams.js
@@ -3,11 +3,6 @@ fetch("https://muc-student-companion-api.vercel.app/api/teams")
   .then((data) => data.forEach(renderToDo)); // Geparste Daten werden weiterverarbeitet
 
 
-  //const crew1 = document.querySelector('.crew1')
-  //const crew2 = document.querySelector('.crew2')
-
-
-
 function renderToDo(team) {  
 
 const teamBlocks = document.querySelector(".element_all")  
@@ -38,49 +33,3 @@ teamHeadline.innerText = "Team "+teamNumber;
    
 }
 
-
-
-
-
-/*
-const crew1 = document.querySelector('.crew1')
-const crew2 = document.querySelector('.crew2')
-
-team1.forEach((teamMember) => {
-  const teamDiv1 = document.createElement('div')
-  const teamDiv2 = document.createElement('div')
-  teamDiv1.classList.add('element_team--name')
-  teamDiv2.classList.add('body-large')
-
-  crew1.appendChild(teamDiv1)
-  teamDiv1.appendChild(teamDiv2)
-  teamDiv2.textContent = teamMember
-
-  if (teamMember === team1[0]) {
-      teamDiv1.classList.add('element--first')
-  }
-  else if (teamMember === team1[team1.length-1]) {
-         teamDiv1.classList.add('element--last')
-           }
-})
-
-
-team2.forEach((teamMember) => {
-        const teamDiv1 = document.createElement('div')
-        const teamDiv2 = document.createElement('div')
-        teamDiv1.classList.add('element_team--name')
-        teamDiv2.classList.add('body-large')
-    
-        crew2.appendChild(teamDiv1)
-        teamDiv1.appendChild(teamDiv2)
-        teamDiv2.textContent = teamMember
-    
-        if (teamMember === team2[0]) {
-            teamDiv1.classList.add('element--first')
-        }
-        else if (teamMember === team2[team2.length-1]) {
-               teamDiv1.classList.add('element--last')
-                 }
-     })
-
-*/
