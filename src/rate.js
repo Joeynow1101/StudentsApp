05962d29ba75858e48a5fe9c2d6714bbd5c3337d
Manuel Verweyen <manuel.verweyen@gmail.com>
2021-12-05 15:55:26 +0100

    delete useless add. closing tag in journal.html

--- src/rate.js
+++ src/rate.js
@@ -2,8 +2,6 @@
 
 const allStars = document.querySelectorAll(".star-unfilled");
 
-
-
 allStars.forEach((starButton, currentStarIndex) => {
   starButton.addEventListener("click", () => {
     const currentStar = currentStarIndex;
@@ -15,17 +13,14 @@ allStars.forEach((starButton, currentStarIndex) => {
     allStars.forEach((fillStar, fillStarIndex) => {
       if (fillStarIndex <= currentStar) {
         fillStar.classList.add("star-filled");
-      };
+      }
     });
   });
 });
 
-
 /* ----- Rating Qauder ----- */
 
 const allQuader = document.querySelectorAll(".quader-unfilled");
-
-
 
 allQuader.forEach((quaderButton, currentQuaderIndex) => {
   quaderButton.addEventListener("click", () => {
@@ -38,48 +33,51 @@ allQuader.forEach((quaderButton, currentQuaderIndex) => {
     allQuader.forEach((fillQuader, fillQuaderindex) => {
       if (fillQuaderindex <= currentQuader) {
         fillQuader.classList.add("quader-filled");
-      };
+      }
     });
   });
 });
 
 /* ----- Formular Send or Cancel ----- */
 
-const cancelButton = document.querySelector(".confirmarea__cancel")
-const confirmButton = document.querySelector(".confirmarea__confirm")
-const rateFormular = document.querySelector(".ratingboxinput")
-
+const cancelButton = document.querySelector(".confirmarea__cancel");
+const confirmButton = document.querySelector(".confirmarea__confirm");
+const rateFormular = document.querySelector(".ratingboxinput");
 
 confirmButton.addEventListener("click", () => {
   const ratingData = {};
-ratingData.rating = rateFormular.querySelectorAll(".star-filled").length
-ratingData.comprehension = rateFormular.querySelectorAll(".quader-filled").length
-ratingData.motto = rateFormular.querySelector("#motto").value
-ratingData.notes = rateFormular.querySelector("#notes").value
+  ratingData.rating = rateFormular.querySelectorAll(".star-filled").length;
+  ratingData.comprehension =
+    rateFormular.querySelectorAll(".quader-filled").length;
+  ratingData.motto = rateFormular.querySelector("#motto").value;
+  ratingData.notes = rateFormular.querySelector("#notes").value;
 
-postRateToAPI(ratingData);
-cancelFormular();
+  postRateToAPI(ratingData);
+  cancelFormular();
 
-function postRateToAPI(rating) {
-  fetch("https://muc-student-companion-api.vercel.app/api/journals",
-  {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(rating)
-   })
-    .then((res) => res.json())
-    .then((entries) => console.log(entries));
+  function postRateToAPI(rating) {
+    fetch("https://muc-student-companion-api.vercel.app/api/journals", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(rating),
+    })
+      .then((res) => res.json())
+      .then((entries) => console.log(entries));
   }
-})
+});
 
-cancelButton.addEventListener("click", () => cancelFormular())
+cancelButton.addEventListener("click", () => cancelFormular());
 
-function cancelFormular (){
-allQuader.forEach((resetQuader, resetQuaderIndex) => {
-  if (resetQuaderIndex != 0) {resetQuader.classList.remove("quader-filled")}
-})
-allStars.forEach((resetStar, resetStarIndex) => {
-  if (resetStarIndex != 0) {resetStar.classList.remove("star-filled")}
-})
-rateFormular.reset()
+function cancelFormular() {
+  allQuader.forEach((resetQuader, resetQuaderIndex) => {
+    if (resetQuaderIndex != 0) {
+      resetQuader.classList.remove("quader-filled");
+    }
+  });
+  allStars.forEach((resetStar, resetStarIndex) => {
+    if (resetStarIndex != 0) {
+      resetStar.classList.remove("star-filled");
+    }
+  });
+  rateFormular.reset();
 }
